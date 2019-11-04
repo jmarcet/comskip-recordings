c42383c93757770cfcbf018617168e739a210b44
Javier Marcet <javier@marcet.info>
2019-11-04 18:49:53 +0100

    Initial commit

---
+++ comskip-recordings.py
@@ -0,0 +1,62 @@
+#!env python3
+
+import asyncio
+import os
+from asyncio.subprocess import DEVNULL, PIPE
+
+COMSKIP_INI = os.environ['HOME'] + '/comskip.ini'
+RECORDINGS  = '/storage/recordings'
+
+COMSKIP     = '/usr/local/bin/comskip'
+INOTIFYWAIT = '/usr/bin/inotifywait'
+MKVMERGE    = '/usr/bin/mkvmerge'
+
+async def run(*args, _bg=False):
+    if _bg:
+        p = await asyncio.create_subprocess_exec(*args, stdout=DEVNULL, stderr=DEVNULL)
+    else:
+        p = await asyncio.create_subprocess_exec(*args, stdout=PIPE, stderr=PIPE)
+        return await p.wait()
+
+async def mainline():
+    proc = await asyncio.create_subprocess_exec(INOTIFYWAIT, '-m', '-r', '-e', 'close_write',
+                                                '--format', '%w%f', RECORDINGS, stdout=PIPE)
+
+    while True:
+        line = await proc.stdout.readline()
+        recording = (line.decode()).rstrip()
+
+        if not os.path.exists(recording) or not os.path.isfile(recording):
+            continue
+        if recording.endswith('.mpeg') or recording.endswith('.mpeg-merged'):
+            filename = os.path.splitext(recording)[0]
+        elif recording.endswith('.mkvtoolnix.chapters'):
+            filename = recording.rpartition('.mkvtoolnix.chapters')[0]
+        else:
+            continue
+
+        if recording.endswith('.mpeg'):
+            print('(1/3) Recording FILENAME="%s" ended' % recording)
+            print('    comskip --ini=%s "%s"' % (COMSKIP_INI, recording))
+            await run(COMSKIP, '--ini=%s' % COMSKIP_INI, recording, _bg=True)
+        elif recording.endswith('.mkvtoolnix.chapters'):
+            chapters  = recording
+            merged    = filename + '.mpeg-merged'
+            recording = filename + '.mpeg'
+            print('(2/3) Chapters FILENAME="%s" generated' % chapters)
+            print('    mkvmerge -o "%s" --chapters "%s" "%s"' % (merged, chapters, recording))
+            await run(MKVMERGE, '-o', merged, '--chapters', chapters, recording, _bg=True)
+        elif recording.endswith('.mpeg-merged'):
+            chapters  = filename + '.mkvtoolnix.chapters'
+            merged    = recording
+            recording = filename + '.mpeg'
+            print('(3/3) Commercial cutpoints FILENAME="%s" merged succesfully' % merged)
+            print('    mv "%s" "%s"' % (merged, recording))
+            os.rename(merged, recording)
+            for delme in [chapters, filename + '.log', filename + '.vdr']:
+                os.remove(delme)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(mainline())
+loop.stop()
+loop.close()
