0e6acafa905eff0b80de760958becd201d5e80cf
Javier Marcet <javier@marcet.info>
2019-11-10 21:55:39 +0100

    Use ionice & nice plus new log function to log all messages to LOGFILE
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- comskip-recordings.py
+++ comskip-recordings.py
@@ -5,23 +5,31 @@ import os
 from asyncio.subprocess import DEVNULL, PIPE
 from glob import glob
 
-COMSKIP_INI = os.environ['HOME'] + '/comskip.ini'
+COMSKIP_INI = '/etc/comskip.ini'
+LOGFILE = '/var/log/comskip.log'
 RECORDINGS = '/storage/recordings'
 
 COMSKIP = '/usr/local/bin/comskip'
 INOTIFYWAIT = '/usr/bin/inotifywait'
+IONICE = '/usr/bin/ionice'
 MKVMERGE = '/usr/bin/mkvmerge'
+NICE = '/usr/bin/nice'
 
+
+def log(text):
+    print(text)
+    with open(LOGFILE, 'a') as f:
+        f.write(text + '\n')
 
 def cleanup(filename, _check=True):
     [os.remove(x) for x in glob(filename + '.*') if not x.endswith('.txt') and not x.endswith('.mpeg')]
     if _check and (not os.path.exists(filename + '.txt') or not os.path.getsize(filename + '.txt')):
         if os.path.exists(filename + '.mpeg'):
-            print('[WARNING]: something went wrong analyzing %s.mpeg, marking as already processed' % filename)
+            log('[WARNING]: something went wrong analyzing %s.mpeg, marking as already processed' % filename)
             with open(filename + '.txt', 'w') as f:
                 f.write('[WARNING]: something went wrong analyzing this video\n')
         else:
-            print('[WARNING]: something went wrong analyzing %s.mpeg' % filename)
+            log('[WARNING]: something went wrong analyzing %s.mpeg' % filename)
 
 
 async def run(*args, _filename=None):
@@ -39,47 +47,51 @@ async def main():
     while True:
         recording = (await proc.stdout.readline()).decode().rstrip()
 
-        if not os.path.exists(recording) or not os.path.isfile(recording):
-            continue
         if recording.endswith('.mpeg') or recording.endswith('.mpeg-merged'):
             filename = os.path.splitext(recording)[0]
         elif recording.endswith('.mkvtoolnix.chapters'):
             filename = recording.rpartition('.mkvtoolnix.chapters')[0]
         else:
+            if recording.endswith('.log.txt'):
+                log(recording)
+            continue
+
+        if not os.path.exists(recording) or not os.path.isfile(recording):
+            log('[ERROR] unable to find %s' % recording)
             continue
 
         if recording.endswith('.mpeg'):
             if os.path.exists(filename + '.txt'):
-                #print('(0/0) %s already processed' % recording)
+                #log('(0/0) %s already processed' % recording)
                 continue
-            print('(1/3) Recording FILENAME="%s" ended' % recording)
-            print('    comskip --ini=%s "%s"' % (COMSKIP_INI, recording))
-            asyncio.create_task(run(COMSKIP, '--ini=%s' % COMSKIP_INI, recording, _filename=filename))
+            log('(1/3) Recording FILENAME="%s" ended' % recording)
+            log('    comskip --ini=%s "%s"' % (COMSKIP_INI, recording))
+            asyncio.create_task(run(NICE, '-n', '10', IONICE, '-c', '3', COMSKIP, '--ini=%s' % COMSKIP_INI, recording, _filename=filename))
 
         elif recording.endswith('.mkvtoolnix.chapters'):
             chapters = recording
             merged = filename + '.mpeg-merged'
             recording = filename + '.mpeg'
-            print('(2/3) Chapters FILENAME="%s" generated' % chapters)
+            log('(2/3) Chapters FILENAME="%s" generated' % chapters)
             if os.path.getsize(chapters) == 132:
-                print('    No commercials found, skipping...')
+                log('    No commercials found, skipping...')
                 cleanup(filename, _check=False)
                 continue
-            print('    mkvmerge -o "%s" --chapters "%s" "%s"' % (merged, chapters, recording))
+            log('    mkvmerge -o "%s" --chapters "%s" "%s"' % (merged, chapters, recording))
             asyncio.create_task(run(MKVMERGE, '-o', merged, '--chapters', chapters, recording, _filename=filename))
 
         elif recording.endswith('.mpeg-merged'):
             merged = recording
             recording = filename + '.mpeg'
-            print('(3/3) Commercial cutpoints FILENAME="%s" merged succesfully' % merged)
-            print('    mv "%s" "%s"' % (merged, recording))
+            log('(3/3) Commercial cutpoints FILENAME="%s" merged succesfully' % merged)
+            log('    mv "%s" "%s"' % (merged, recording))
             try:
                 os.rename(merged, recording)
             except:
-                print('      -> FAILED: could not move "%s" to "%s"' % (merged, recording))
+                log('      -> FAILED: could not move "%s" to "%s"' % (merged, recording))
             cleanup(filename)
 
 try:
     asyncio.run(main())
 except KeyboardInterrupt:
-    print('Good bye!')
+    log('Good bye!')
